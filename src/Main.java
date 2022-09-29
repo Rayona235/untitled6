cc89e567b2096336475d593668ad55d3b79156d8
Acer <atabekovaayona@gmail.com>
2022-09-29 17:06:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static String main(String[] args) {
+     String canGoOutside(int age, temperature);
+        if(age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30);
+        return" Можно идти погулять ";
+        else if (age <20 && temperature >= 0 && temperature <= 28);
+        return" Можно идти погулять ";
+        else if (age > 45 && temperature >= -10 && temperature <= 25);
+        return" Можно идти погулять ";
+        else;
+            return" Оставайтесь дома ";
+
+
+
+
+
+    }
+}
